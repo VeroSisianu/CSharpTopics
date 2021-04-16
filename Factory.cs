6d1ff4db04e051ae55e7814a334f4fc5b63c6864
VeroSisianu <nica220196@gmail.com>
2021-04-16 18:02:56 +0300

    Lesson1Git

---
+++ Factory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lectia23_1
+{
+    class Factory
+    {
+        string name;
+        int postcode;
+        string street;
+        public Factory(string name, int postcode, string street)
+        {
+            this.name = name;
+            this.postcode = postcode;
+            this.street = street;
+        }
+        public class Address
+        {
+            public string GetAddress(Factory factory)
+            {
+
+                return $"Factory {factory.name} is on {factory.street} {factory.postcode}";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lectia23_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new Factory("Alex's factory", 5498, "Alexei Mateevici 1");
+            Factory.Address addressOfFactory = new Factory.Address();
+            Console.WriteLine(addressOfFactory.GetAddress(factory));
+        }
+    }
+}
